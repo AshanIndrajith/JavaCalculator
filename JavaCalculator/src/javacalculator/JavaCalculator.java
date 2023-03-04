5bb6f654f3ee4b0932818000ef2b5fe1467371ed
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-03-04 09:46:50 +0530

    update

---
+++ JavaCalculator/src/javacalculator/JavaCalculator.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javacalculator;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author INSIGHT
+ */
+public class JavaCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+    
+    
+    
+    }
+    
+}
+
+class clabody extends JFrame{
+
+    public clabody() {
+        
+        setVisible(true);
+        setTitle("My Cal");
+        setSize(WIDTH,HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        
+        
+        
+    }
+    
+     
+
+
+}
